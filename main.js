1bb6c1ee7f5f7fe2e01b5c74d49753b98d4bfeb8
Dan Glick <danjglick@gmail.com>
2021-01-21 03:45:58 -0500

    replace all rectangles with images

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+// make responsive
+// implement game mechanics
+// implement levels
+// implement sounds
+// implement global high score
+// polish
+// port to ios
+// release
+
 let canvas;
 let context;
 let level = 1
@@ -5,78 +14,58 @@ let planets = []
 let asteroids = []
 let xPositionProtagonist;
 let yPositionProtagonist;
-let MILLISECONDS_PER_FRAME = 100
-let PIXELS_ASTEROIDS_TRAVEL_PER_FRAME = 20
-let PIXELS_PROTAGONIST_TRAVELS_PER_CLICK = 100
+const MILLISECONDS_PER_FRAME = 100
+const PIXELS_ASTEROIDS_TRAVEL_PER_FRAME = 20
+const PIXELS_PROTAGONIST_TRAVELS_PER_CLICK = 50
+const PLANET_IMAGES = ["mars.png", "moon.png", "rainbow_planet.png", "sunset_planet.png", "golden_planet.png"]
+const ASTEROID_IMAGES = ["asteroid_a.png", "asteroid_b.png"]
 
 function start_game() {
     canvas = document.getElementsByTagName("canvas")[0]
     canvas.width = window.innerWidth - 10
     canvas.height = window.innerHeight - 110
     context = canvas.getContext("2d")
-    initialize_level()
+    initializeLevel()
     gameLoop()
 }
 
-function initialize_level() {
-    initialize_planets()
+function initializeLevel() {
+    initializePlanets()
     xPositionProtagonist = planets[0]["xPosition"]
     yPositionProtagonist = planets[0]["yPosition"]
 }
 
-function initialize_planets() {
-    for (let planet_counter = 1; planet_counter <= level + 2; planet_counter++) {
-        let xPositionPlanet;
-        let yPositionPlanet;
-        let lengthPlanet;
+function initializePlanets() {
+    let images = PLANET_IMAGES
+    for (let i = 1; i <= level + 2; i++) {
+        let xPosition;
+        let yPosition;
         let try_again = true
         while (try_again) {
             try_again = false
-            xPositionPlanet = Math.floor(Math.random() * (canvas.width - 200))
-            yPositionPlanet = Math.floor(Math.random() * (canvas.height - 200))
-            lengthPlanet = Math.floor(Math.random() * 50) + 150
-            // if (planets.length > 0) {
-            //     for (let i = 0; i < planets.length; i++) {
-            //         let is_enough_width_between_planets = Math.abs(xPositionPlanet - planets[i]["xPosition"]) > 250
-            //         let is_enough_height_between_planets = Math.abs(yPositionPlanet - planets[i]["yPosition"]) > 250
-            //         if (!is_enough_width_between_planets || !is_enough_height_between_planets) {
-            //             try_again = true
-            //         }
-            //     }
-            // }
+            xPosition = Math.floor(Math.random() * (canvas.width - 200))
+            yPosition = Math.floor(Math.random() * (canvas.height - 200))
+            if (planets.length > 0) {
+                for (let i = 0; i < planets.length; i++) {
+                    let isEnoughWidthBetweenPlanets = Math.abs(xPosition - planets[i]["xPosition"]) > 300
+                    let isEnoughHeightBetweenPlanets = Math.abs(yPosition - planets[i]["yPosition"]) > 150
+                    if (!isEnoughWidthBetweenPlanets || !isEnoughHeightBetweenPlanets) {
+                        try_again = true
+                    }
+                }
+            }
         }
+        let length = Math.floor(Math.random() * 200) + 100
+        let imageIndex = Math.floor(Math.random() * images.length)
+        let image = images[imageIndex]
+        images.splice(imageIndex, 1)
+        let imageSrc = `images/${image}`
         planets.push({
-            "xPosition": xPositionPlanet,
-            "yPosition": yPositionPlanet,
-            "length": lengthPlanet
+            "xPosition": xPosition,
+            "yPosition": yPosition,
+            "length": length,
+            "imageSrc": imageSrc
         })
-    }
-}
-
-function gameLoop() {
-    context.clearRect(0, 0, canvas.width, canvas.height)
-    drawPlanets()
-    drawAsteroids()
-    drawProtagonist()
-    document.addEventListener('keydown', moveProtagonist)
-    setTimeout(gameLoop, MILLISECONDS_PER_FRAME)
-}
-
-function drawPlanets() {
-    for (let i = 0; i < planets.length; i++) {
-        context.fillStyle = "green"
-        context.fillRect(planets[i]["xPosition"], planets[i]["yPosition"], planets[i]["length"], planets[i]["length"])
-    }
-}
-
-function drawAsteroids() {
-    let newAsteroid = initializeAsteroid()
-    asteroids.push(newAsteroid)
-    for (let i = 0; i < asteroids.length; i++) {
-        let asteroid = asteroids[i]
-        positionAsteroid(asteroid)
-        context.fillStyle = "grey"
-        context.fillRect(asteroid["xPosition"], asteroid["yPosition"], asteroid["length"], asteroid["length"])
     }
 }
 
@@ -89,12 +78,53 @@ function initializeAsteroid() {
         if (xPosition === 0 || xPosition === canvas.width || yPosition === 0 || yPosition === canvas.height) {
             let possibleDirections = ["northeast", "southeast", "southwest", "northwest"]
             let direction = possibleDirections[Math.floor(Math.random() * possibleDirections.length)]
+            let length = Math.floor(Math.random() * 60) + 15
+            let randomAsteroidImage = ASTEROID_IMAGES[Math.floor(Math.random() * ASTEROID_IMAGES.length)]
+            let imageSrc = `images/${randomAsteroidImage}`
             return {
                 "xPosition": xPosition,
                 "yPosition": yPosition,
                 "direction": direction,
-                "length": Math.floor(Math.random() * 60) + 15}
+                "length": length,
+                "imageSrc": imageSrc
+            }
         }
+    }
+}
+
+function gameLoop() {
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    drawPlanets()
+    drawProtagonist()
+    drawAsteroids()
+    document.addEventListener('keydown', moveProtagonist)
+    setTimeout(gameLoop, MILLISECONDS_PER_FRAME)
+}
+
+function drawPlanets() {
+    for (let i = 0; i < planets.length; i++) {
+        let planet = planets[i]
+        let element = document.createElement("IMG")
+        element.src = planet.imageSrc
+        context.drawImage(element, planet.xPosition, planet.yPosition, planet.length, planet.length)
+    }
+}
+
+function drawProtagonist() {
+    element = document.createElement("IMG")
+    element.src = "images/spaceship.png"
+    context.drawImage(element, xPositionProtagonist, yPositionProtagonist, 60, 45)
+}
+
+function drawAsteroids() {
+    let newAsteroid = initializeAsteroid()
+    asteroids.push(newAsteroid)
+    for (let i = 0; i < asteroids.length; i++) {
+        let asteroid = asteroids[i]
+        positionAsteroid(asteroid)
+        let element = document.createElement("IMG")
+        element.src = asteroids[i].imageSrc
+        context.drawImage(element, asteroid["xPosition"], asteroid["yPosition"], asteroid["length"], asteroid["length"])
     }
 }
 
@@ -116,11 +146,6 @@ function positionAsteroid(asteroid) {
             asteroid["xPosition"] -= PIXELS_ASTEROIDS_TRAVEL_PER_FRAME
             asteroid["yPosition"] -= PIXELS_ASTEROIDS_TRAVEL_PER_FRAME
     }
-}
-
-function drawProtagonist() {
-    context.fillStyle = "magenta"
-    context.fillRect(xPositionProtagonist, yPositionProtagonist, 25, 25)
 }
 
 function moveProtagonist(event) {
